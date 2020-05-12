50344813d3a6bb1dd23fd270383dba13a7091008
VIJAY GUPTA <vijaygupta131999@gmail.com>
2020-05-12 13:06:00 +0530

    first commit

---
+++ src/dbtransaction/DBTransaction.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dbtransaction;
+
+/**
+ *
+ * @author USER
+ */
+public class DBTransaction {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
