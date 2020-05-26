0e76b6ddd78c06abf94f121250859e95ae0e929a
VIJAY GUPTA <vijaygupta131999@gmail.com>
2020-05-27 01:55:57 +0530

    Oracle to MySQL Types Mapping function is defined. ColumnDetails class will store the column description that is retrieved from source table to create new table.

--- src/dbtransaction/RunningTransaction.java
+++ src/dbtransaction/RunningTransaction.java
@@ -18,17 +18,17 @@ import javafx.util.converter.LocalDateStringConverter;
  */
 public class RunningTransaction {
   
-    private final String source="PostgreSql";
-    private final String sUserName="postgres";
-    private final String sPassword="postmanager";
+    private final String source="Oracle";
+    private final String sUserName="system";
+    private final String sPassword="manager";
     private final String sHost="";
     private final String sPort="";
     private final String sDataBaseName="";
-    private final String sTableName="users";
+    private final String sTableName="uuemp";
 
-    private final String destination="Oracle";
-    private final String dUserName="system";
-    private final String dPassword="manager";
+    private final String destination="MySQL";
+    private final String dUserName="root";
+    private final String dPassword="mymanager";
     private final String dHost="";
     private final String dPort="";
     private final String dDataBaseName="";
@@ -65,24 +65,35 @@ public class RunningTransaction {
             System.out.println("problem with destination connection");
             return;
         }
-        DatabaseMetaData d=sConnection.getMetaData();
-        ResultSet columns = d.getColumns(null,null, sTableName, null);
+       // System.out.println(1+""+sConnection.getSchema());
+       
+        ResultSetMetaData d=sConnection.createStatement().executeQuery("select *from uuemp where 1=2").getMetaData();
+       
         
         
-while(columns.next())
+for(int i=1;i<=d.getColumnCount();i++)
 {
-    String columnName = columns.getString("COLUMN_NAME");
-    String type = columns.getString("TYPE_NAME");
-    int columnsize = Integer.parseInt(columns.getString("COLUMN_SIZE"));
-    String defaultvalue = columns.getString("COLUMN_DEF");
-    String isNullable = columns.getString("IS_NULLABLE");
-    String is_autoIncrment = columns.getString("IS_AUTOINCREMENT");
+    String columnName = d.getColumnName(i);
+    String type = d.getColumnTypeName(i);
+    int columnsize = (d.getColumnDisplaySize(i));
+    int  columnSqlType = d.getColumnType(i);
+    int precision= d.getPrecision(i);
+    int scale= d.getScale(i);
     //Printing results
-    System.out.println(columnName+""+type+""+columnsize+""+defaultvalue+""+isNullable+""+is_autoIncrment);
-    coldetail.add(new ColumnDetails(columnName, type, columnsize, defaultvalue, isNullable, is_autoIncrment));
+    System.out.println(columnName+""+type+""+columnsize+""+columnSqlType+""+precision+""+scale);
+    coldetail.add(new ColumnDetails(columnName,  columnsize, type,precision, scale));
+}
+ System.out.println("************************************************************************");
+new DataTypeMapping().oracletoMysql(coldetail);
+
+
+String query="";
+for(int i=0;i<coldetail.size();i++){
+    System.out.println(coldetail.get(i).columnName+""+coldetail.get(i).datatype+""+coldetail.get(i).columnsize+""+coldetail.get(i).precision+""+coldetail.get(i).scale);
 }
 
-        System.out.println("");
+
+       
     }
   
 }
